ff55e54e5c91093f1792366179144ac0d0d4fe12
cocbase <85095407+cocbase@users.noreply.github.com>
2024-02-14 23:33:39 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,35 @@
+# YTS_Specific_Movies_Notifier
+
+Sents a discord alert when a matching movie title in `movies_titles.txt` is found in YTS.
+
+1. Clone Repository
+
+```sh
+$ git clone https://github.com/nabzter/YTS_Specific_Movies_Notifier.git
+```
+
+2. Move into the directory
+
+```sh
+$ cd YTS_Specific_Movies_Notifier
+```
+
+3. Install Dependencies
+```sh
+$ pip install -r requirements.txt
+```
+
+4. Add your discord webhook URL to `webhook_url` variable.
+
+5. Run Script
+```sh
+$ python main.py
+```
+
+Discord Alert Structure
+```
+@everyone
+ Latest movie: The Beekeeper
+ Date: 02-02-2024
+ URL: https://yts.mx/https://yts.mx/movies/the-beekeeper-2024
+```
---
+++ main.py
@@ -0,0 +1,63 @@
+import requests
+from bs4 import BeautifulSoup
+from discord_webhook import DiscordWebhook
+import time
+import pytz
+from datetime import datetime
+import re
+
+url = "https://yts.mx/"
+webhook_url = "webhook_url"
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
+
+current_date = datetime.now().strftime("%d.%m.%y")
+
+def read_title_names():
+    with open("movies_titles.txt", "r") as file:
+        titles = set()
+        for line in file:
+            cleaned_title = line.strip().lower().replace(":", "")
+            titles.add(cleaned_title)
+        return titles
+
+def scrape_latest_titles():
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, "html.parser")
+    
+    latest_titles = []
+    for movie in soup.select(".browse-movie-wrap a.browse-movie-title")[:4]:
+        title = re.sub(r'\[.*?\]', '', movie.text.strip().lower().replace(":", "").replace(" ", "", 1))
+        movie_url = url + movie['href']
+        latest_titles.append({"title": title, "url": movie_url})    
+    return latest_titles
+
+def send_to_discord(title, url):
+    DiscordWebhook(url=webhook_url, content=f"@everyone\n Latest movie: **{title}**\n Date: {current_date}\n URL: {url}").execute()
+    print(f"Sent to Discord: {title}")
+
+def remove_matching_title(title):
+    with open("movies_titles.txt", "r+") as file:
+        lines = file.readlines()
+        file.seek(0)
+        file.writelines(line for line in lines if line.strip() != title)
+        file.truncate()
+
+while True:
+    latest_movies = scrape_latest_titles()
+    stored_titles = read_title_names()
+
+    print("Checking Movies...")
+
+    for movie in latest_movies:
+        title = movie["title"]
+        url = movie["url"]
+        if title in stored_titles:
+            print(f"Match found: {title}")
+            send_to_discord(title.title(), url)
+            remove_matching_title(title)
+            print(f"Removed from the file: {title}")
+        
+    print("No Matching Movies Found")
+
+    print("Waiting 15 Mins...")
+    time.sleep(900)
